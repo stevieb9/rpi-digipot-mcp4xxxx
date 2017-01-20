556571c54b86ce52e84ef0d5b173273bfc5c60fe
Steve Bertrand <steveb@cpan.org>
2017-01-20 11:05:39 -0700

    prepared module file for code

--- lib/RPi/DigiPot/MCP4XXXX.pm
+++ lib/RPi/DigiPot/MCP4XXXX.pm
@@ -1,99 +1,37 @@
 package RPi::DigiPot::MCP4XXXX;
 
-use 5.006;
+use warnings;
 use strict;
-use warnings FATAL => 'all';
-
-=head1 NAME
-
-RPi::DigiPot::MCP4XXXX - The great new RPi::DigiPot::MCP4XXXX!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
 
 our $VERSION = '0.01';
 
+1;
+__END__
+
+=head1 NAME
+
+RPi::DigiPot::MCP4XXXX - Interface to the MCP4xxxx series digital potentiometers
+on the Raspbery Pi
+
+=head1 DESCRIPTION
+
+This distribution allows you to interface directly with the MCP41xxx and
+MCP42xxx series digital potentiomenters attached to the SPI bus on the
+Raspberry Pi.
+
+The MCP41xxx units have a single built-in potentiometer, where the MCP42xxx
+units have two.
+
+Both series will operate on either 3.3V or 5V, as the potentiometers do not send
+anything back to the Pi's GPIO.
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use RPi::DigiPot::MCP4XXXX;
-
-    my $foo = RPi::DigiPot::MCP4XXXX->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+=head1 METHODS
 
 =head1 AUTHOR
 
 Steve Bertrand, C<< <steveb at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-rpi-digipot-mcp4xxxx at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=RPi-DigiPot-MCP4XXXX>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc RPi::DigiPot::MCP4XXXX
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=RPi-DigiPot-MCP4XXXX>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/RPi-DigiPot-MCP4XXXX>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/RPi-DigiPot-MCP4XXXX>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/RPi-DigiPot-MCP4XXXX/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
 
 =head1 LICENSE AND COPYRIGHT
 
@@ -105,7 +43,3 @@ by the Free Software Foundation; or the Artistic License.
 
 See L<http://dev.perl.org/licenses/> for more information.
 
-
-=cut
-
-1; # End of RPi::DigiPot::MCP4XXXX
